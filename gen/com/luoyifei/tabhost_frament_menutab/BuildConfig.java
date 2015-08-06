82aa563249475e36a8e6cebd0c915edb668ecf79
gdutyifei <gdutyifei@163.com>
2015-08-07 00:49:25 +0800

    将TabHost和Fragment的样式首次提交

---
+++ gen/com/luoyifei/tabhost_frament_menutab/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.luoyifei.tabhost_frament_menutab;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
