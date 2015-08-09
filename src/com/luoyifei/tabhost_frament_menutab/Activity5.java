e407b58bb179d63ef608c8879c7f7feda839bb4b
gdutyifei <gdutyifei@163.com>
2015-08-09 23:50:12 +0800

    增加几个Activity

---
+++ src/com/luoyifei/tabhost_frament_menutab/Activity5.java
@@ -0,0 +1,12 @@
+package com.luoyifei.tabhost_frament_menutab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity5 extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
